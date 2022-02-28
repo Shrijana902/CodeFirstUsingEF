08565fb4e63e269380680804deb26bb0579c9709
Shrijana Basnet <basnetshrijana902@gmail.com>
2022-02-28 14:18:22 +0545

    Oderby

--- Program.cs
+++ Program.cs
@@ -1,29 +1,37 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+
 namespace CodeFirstDemo
 {
-    public class Program
+    public class Program:DbContext
     {
-        public static void Main(string[] args)
+         static void Main(string[] args)
         {
-            IList<Student> newStudents = new List<Student>() {
-                                    new Student() { Name = "Steve" },
-                                    new Student() { Name = "Bill" },
-                                    new Student() { Name = "James" }
-                                };
-             IList<Course> newcourses = new List<Course>() {
-                                    new Course() { CourseName = "c" },
-                                    new Course() { CourseName = "VB" },
-                                     new Course() { CourseName = "C+" }
-                                };
-            using (var context = new SchoolContext())
-            {
-                context.Students.AddRange(newStudents);
-                context.Courses.AddRange(newcourses);
-                context.SaveChanges();
-                var student=context.Students.ToList();
-                foreach(var s in student)
+            // IList<Student> newStudents = new List<Student>() {
+            //                         new Student() { Name = "Steve" },
+            //                         new Student() { Name = "Bill" },
+            //                         new Student() { Name = "James" }
+            //                     };
+            //  IList<Course> newcourses = new List<Course>() {
+            //                         new Course() { CourseName = "c" },
+            //                         new Course() { CourseName = "VB" },
+            //                          new Course() { CourseName = "C+" }
+            //                     };
+             using(var context = new SchoolContext())
+             {
+             
+            //     context.Students.AddRange(newStudents);
+            //     context.Courses.AddRange(newcourses);
+            //     context.SaveChanges();
+                var student=from s in context.Students
+                            orderby s.Name descending
+                            select s;
+                foreach(var groupItem in student)
                 {
-                    Console.WriteLine(s.Name);
+                    Console.WriteLine(groupItem.Name);
+                    
+                
+                    
                 }
                 
                 

a8a80c1cad976dee95131afbd85578980482fdea
Shrijana Basnet <basnetshrijana902@gmail.com>
2022-03-02 12:39:51 +0545

    Anonymous object result

--- Program.cs
+++ Program.cs
@@ -3,42 +3,31 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CodeFirstDemo
 {
-    public class Program:DbContext
+    public class Program
     {
-         static void Main(string[] args)
+        static void Main(string[] args)
         {
-            // IList<Student> newStudents = new List<Student>() {
-            //                         new Student() { Name = "Steve" },
-            //                         new Student() { Name = "Bill" },
-            //                         new Student() { Name = "James" }
-            //                     };
-            //  IList<Course> newcourses = new List<Course>() {
-            //                         new Course() { CourseName = "c" },
-            //                         new Course() { CourseName = "VB" },
-            //                          new Course() { CourseName = "C+" }
-            //                     };
-             using(var context = new SchoolContext())
-             {
-             
-            //     context.Students.AddRange(newStudents);
-            //     context.Courses.AddRange(newcourses);
-            //     context.SaveChanges();
-                var student=from s in context.Students
-                            orderby s.Name descending
-                            select s;
-                foreach(var groupItem in student)
+
+            using (var context = new SchoolContext())
+            {
+                var data = from s in context.Students
+                                where s .StudentId==6
+                                select new {
+                                    name=s.Name
+                                };
+                    
+                foreach (var item in data)
                 {
-                    Console.WriteLine(groupItem.Name);
-                    
-                
-                    
+                    Console.WriteLine(item.name);
                 }
-                
-                
 
             }
-            
-        
+
+
+
         }
+
+
     }
 }
+
